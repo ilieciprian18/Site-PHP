47395bddd1b7f7559c061206a732dedfba4fc06b
ilieciprian18 <61537857+ilieciprian18@users.noreply.github.com>
2020-11-03 18:40:18 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,31 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Bine ati venit la sectiunea "Despre" !</p>'; ?> 
+ <ul>
+  <li>Am folosit freehostia pentru a hosta site-ul</li>
+  <li>"http://phpsite.space/test.php"</li>
+  <li>Am cumparat de asemenea si un domeniu pentru a putea fi accesat usor la adresa phpsite.space</phpsite></li>
+  <li>Contul de github pe care se afla codul este : https://github.com/ilieciprian18/Site-PHP</li>
+  <li>Tema site-ului este "Activitatile unei firme de comert"</li>
+  <li>M-am gandit sa dezvolt o aplicatie cu 2 categorii de useri : utilizatori si manageri</li>
+  <li>UTILIZATORII vor fi cei care se vor inscrie in excursii</li>
+  <li>MANAGERII vor fi cei care vor organiza excursii</li>
+  <li>---Se vor mai adauga categorii de useri daca va fi nevoie---</li>
+</ul>
+<ul>
+  <li>Schita TEMPLATE REGISTER FORM (beta); </li>
+</ul>
+<img src="http://phpsite.space/login.png" >
+<ul>
+  <li>Schita TEMPLATE LOGIN FORM (beta); </li>
+</ul>
+<img src="http://phpsite.space/loginn.png" >
+<ul>
+  <li>Schita TEMPLATE DIAGRAMA BD (beta) </li>
+</ul>
+<img src="http://phpsite.space/DIagrama BD.png" >
+ </body>
+</html>
